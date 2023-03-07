0dfd4e6dbe734a644ad5ebfc39528fb81262a468
Amanda <107001164+amandadeusilva@users.noreply.github.com>
2023-03-07 14:39:01 -0300

    commit inicial

---
+++ assets/javaScript/etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                nome: 'João da Silva',
+                partido: 'ABC',
+                fotos:[
+                    {url:'assets/imagem/joao.jpg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '77222',
+                nome: 'Felipe de Souza',
+                partido: 'DEFG',
+                fotos:[
+                    {url:'assets/imagem/felipe.jpg', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '99',
+                nome: 'Aline Carvalho',
+                partido: 'ABC',
+                vice: 'Cic da praça',
+                fotos: [
+                    {url:'assets/imagem/prefeita.jpg', legenda: 'Prefeito'},
+                    {url:'assets/imagem/vice1.jpg', legenda: 'Vice: Cique da praca'}
+                ]
+            },
+            {
+                numero: '88',
+                nome:'Deyvid Boeno',
+                partido: 'Qwerty',
+                vice: 'João Cabral',
+                fotos: [
+                    {url: 'assets/imagem/prefeito.jpg', legenda: 'Prefeito'},
+                    {url: 'assets/imagem/vice2.jpg',legenda: 'Vice: João Cabral'}
+                ]
+            },
+        ]
+    }
+];
